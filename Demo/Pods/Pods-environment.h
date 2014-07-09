2e580a40b05c4fc6e53eeb337b1435bf4265d53c
利辺羅エルネスト <rivera_ernesto@cyberagent.co.jp>
2014-07-09 15:59:09 +0900

    Update Demo

--- Demo/Pods/Pods-environment.h
+++ Demo/Pods/Pods-environment.h
@@ -9,26 +9,26 @@
 // CocoaLumberjack
 #define COCOAPODS_POD_AVAILABLE_CocoaLumberjack
 #define COCOAPODS_VERSION_MAJOR_CocoaLumberjack 1
-#define COCOAPODS_VERSION_MINOR_CocoaLumberjack 8
+#define COCOAPODS_VERSION_MINOR_CocoaLumberjack 9
 #define COCOAPODS_VERSION_PATCH_CocoaLumberjack 1
 
 // CocoaLumberjack/Core
 #define COCOAPODS_POD_AVAILABLE_CocoaLumberjack_Core
 #define COCOAPODS_VERSION_MAJOR_CocoaLumberjack_Core 1
-#define COCOAPODS_VERSION_MINOR_CocoaLumberjack_Core 8
+#define COCOAPODS_VERSION_MINOR_CocoaLumberjack_Core 9
 #define COCOAPODS_VERSION_PATCH_CocoaLumberjack_Core 1
 
 // CocoaLumberjack/Extensions
 #define COCOAPODS_POD_AVAILABLE_CocoaLumberjack_Extensions
 #define COCOAPODS_VERSION_MAJOR_CocoaLumberjack_Extensions 1
-#define COCOAPODS_VERSION_MINOR_CocoaLumberjack_Extensions 8
+#define COCOAPODS_VERSION_MINOR_CocoaLumberjack_Extensions 9
 #define COCOAPODS_VERSION_PATCH_CocoaLumberjack_Extensions 1
 
 // LumberjackConsole
 #define COCOAPODS_POD_AVAILABLE_LumberjackConsole
 #define COCOAPODS_VERSION_MAJOR_LumberjackConsole 2
 #define COCOAPODS_VERSION_MINOR_LumberjackConsole 0
-#define COCOAPODS_VERSION_PATCH_LumberjackConsole 1
+#define COCOAPODS_VERSION_PATCH_LumberjackConsole 2
 
 // NBUCore
 #define COCOAPODS_POD_AVAILABLE_NBUCore
@@ -40,5 +40,5 @@
 #define COCOAPODS_POD_AVAILABLE_NBULog
 #define COCOAPODS_VERSION_MAJOR_NBULog 1
 #define COCOAPODS_VERSION_MINOR_NBULog 0
-#define COCOAPODS_VERSION_PATCH_NBULog 7
+#define COCOAPODS_VERSION_PATCH_NBULog 8
 
