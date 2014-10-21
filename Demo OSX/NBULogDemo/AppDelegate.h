fdf631b6b5b25b574be1670922a03dccc0dff4a7
Ernesto Rivera <rivera_ernesto@cyberagent.co.jp>
2014-10-21 11:17:33 +0900

    Finish OS X Demo

--- Demo OSX/NBULogDemo/AppDelegate.h
+++ Demo OSX/NBULogDemo/AppDelegate.h
@@ -10,6 +10,9 @@
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
+@property (weak, nonatomic) IBOutlet NSWindow * window;
+
+- (IBAction)buttonClicked:(NSButton *)sender;
 
 @end
 
--- Demo OSX/NBULogDemo/AppDelegate.m
+++ Demo OSX/NBULogDemo/AppDelegate.m
@@ -7,20 +7,58 @@
 //
 
 #import "AppDelegate.h"
-
-@interface AppDelegate ()
-
-@property (weak) IBOutlet NSWindow *window;
-@end
+#import "MockNetworkModule.h"
 
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Set log level for a specific module
+    [NBULog setAppLogLevel:LOG_LEVEL_INFO
+                 forModule:APP_MODULE_NETWORK];
+    
+    // Test logs from C functions
+    testCLogs();
+    
+    // Test log modules
+    [MockNetworkModule generateMockMessages];
 }
 
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
+- (IBAction)buttonClicked:(NSButton *)sender
+{
+    switch (sender.tag)
+    {
+        case 0:
+            NBULogTrace();
+            break;
+        case 1:
+            NBULogVerbose(@"Test message");
+            break;
+        case 2:
+            NBULogDebug(@"Test message");
+            break;
+        case 3:
+            NBULogInfo(@"Test message");
+            break;
+        case 4:
+            NBULogWarn(@"Test message");
+            break;
+        default:
+            NBULogError(@"Test message");
+            break;
+    }
+}
+
+#pragma mark - Logs from C functions
+
+void testCLogs()
+{
+    NBULogTrace();
+    NBULogVerbose(@"Verbose message from a C function");
+    NBULogInfo(@"Info message from a C function");
+    NBULogWarn(@"Warning message from a C function");
+    NBULogError(@"Error message from a C function");
 }
 
 @end
+
